d100527c44d2ac196fdd44be8f8e4c3da7f6e19f
Wraithan (Chris McDonald) <xwraithanx@gmail.com>
2014-11-01 15:22:46 -0700

    more specific

--- motivate.js
+++ motivate.js
@@ -14,19 +14,23 @@ zen.register_commands(
 
 var filtered = zen.filter({version: 1, type: 'directed_privmsg'})
 filtered.on('data', function(msg){
-  var motivate = /^m(.*)/i.exec(msg.data.message)
-  var demotivate = /^dm(.*)/i.exec(msg.data.message)
+  var motivate = /^m( .*|$)/i.exec(msg.data.message)
+  var demotivate = /^dm( .*|$)/i.exec(msg.data.message)
   var validOne = (motivate || demotivate)
   if (validOne) {
     var target = validOne[1].trim() || msg.data.channel
     if (motivate) {
-      zen.send_privmsg(msg.data.channel,
-                       "You're doing great work " +
-                       target + "!");
+      zen.send_privmsg(
+        msg.data.channel,
+        "You're doing great work, " +
+        target + "!"
+      );
     } else if (demotivate) {
-      zen.send_privmsg(msg.data.channel,
-                       "You're doing horrible work " +
-                       target + "!");
+      zen.send_privmsg(
+        msg.data.channel,
+        "You're doing horrible work, " +
+        target + "!"
+      );
     }
   }
 })
